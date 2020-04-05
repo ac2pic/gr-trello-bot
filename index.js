7543d5c7fdb19670a24b20d4e46f4a1ebcf5f836
ac2pic <ac2pic@gmail.com>
2020-04-04 20:34:40 -0500

    No longer showing webhook url

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const express  = require('express'),
     port = process.env.PORT || 3123;
 
 app.use( bodyParser.json() );
-const discordWebhookUrl = 'https://discordapp.com/api/webhooks/696156837963104277/n1AhF8X0YlPxfxTEsGsxwfUjNKaNkAbFr7FRAAkqg2e_XqD2OsciPLIFNGmLRZr3O9DY';
+
+const gifs = [
+	"https://cdn.discordapp.com/attachments/696050162149097613/696129710266253392/tumblr_p4rz82xWrm1vte390o3_500.gif"
+];
 
 function sendDiscordMessage(message) {
-	return fetch(discordWebhookUrl, {
+	return fetch(process.env.webhook_url, {
 		method: "POST",
 		headers: {'Content-Type': 'application/json'},
 		body: JSON.stringify(message)
 	});
 }
+
 
 app.post("/trello", async (req, res, next) => {
 	const {name, url} = req.body;
@@ -21,10 +25,10 @@ app.post("/trello", async (req, res, next) => {
 	await sendDiscordMessage({
 		"embeds": [{
 			"color": 6232278,
-			"title": "Card Complete",
+			"title": "Step Complete",
 			"description": `The [${name}](${url}) step has been completed!`,
 			"image": {
-			  "url": "https://cdn.discordapp.com/attachments/696050162149097613/696129710266253392/tumblr_p4rz82xWrm1vte390o3_500.gif"
+			  "url": gifs[0]
 			}
 		}]
 	});
