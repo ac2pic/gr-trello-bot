0456e22e393382fc431b1973fe99dcd59fe2a1f2
ac2pic <ac2pic@gmail.com>
2020-04-04 21:43:14 -0500

    Support for choosing random gif

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express  = require('express'),
+const express = require('express'),
 	fetch = require('node-fetch'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    port = process.env.PORT || 3123;
+	app = express(),
+	bodyParser = require('body-parser'),
+	port = process.env.PORT || 3123;
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
 const gifs = [
 	"https://cdn.discordapp.com/attachments/696050162149097613/696129710266253392/tumblr_p4rz82xWrm1vte390o3_500.gif"
@@ -13,14 +13,16 @@ const gifs = [
 function sendDiscordMessage(message) {
 	return fetch(process.env.webhook_url.trim(), {
 		method: "POST",
-		headers: {'Content-Type': 'application/json'},
+		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(message)
 	});
 }
 
 
+const chooseRandom = (arr) => arr[parseInt(Math.random() * arr.length)];
+
 app.post("/trello", async (req, res, next) => {
-	const {name, url} = req.body;
+	const { name, url } = req.body;
 
 	await sendDiscordMessage({
 		"embeds": [{
@@ -28,13 +30,13 @@ app.post("/trello", async (req, res, next) => {
 			"title": "Step Complete",
 			"description": `The [${name}](${url}) step has been completed!`,
 			"image": {
-			  "url": gifs[0]
+				"url": chooseRandom(gifs)
 			}
 		}]
 	});
 	res.send('OK');
 });
 
-const server = app.listen(port, function () {
+app.listen(port, function () {
 	console.log('Webhook handler listening on port %s', port);
 });
